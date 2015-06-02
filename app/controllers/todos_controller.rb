5d6d68acd61170b15b1d13676290d87ef788c24a
phitchcock <phitchcock@me.com>
2015-06-01 18:01:56 -0700

    added todo controller and actions

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,55 @@
+class TodosController < ApplicationController
+
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @todo.destroy
+      redirect_to todos_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+
+
+end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,3 @@
 class Todo < ActiveRecord::Base
-
+  validates :name, presence: true
 end
